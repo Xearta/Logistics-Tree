832f797471a8e634fd59f97b274232f01ae89bcd
Xearta <rsmcwow@gmail.com>
2020-11-21 22:02:30 -0500

    Started Modding Tree

---
+++ js/mod.js
@@ -0,0 +1,61 @@
+let modInfo = {
+  name: 'The Colors Tree',
+  id: 'idlecolors',
+  author: 'Xearta',
+  pointsName: 'points',
+  discordName: '',
+  discordLink: '',
+  changelogLink:
+    'https://github.com/Xearta/The-Modding-Tree/blob/master/changelog.md',
+  initialStartPoints: new Decimal(10), // Used for hard resets and new players
+
+  offlineLimit: 1, // In hours
+};
+
+// Set your version in num and name
+let VERSION = {
+  num: '0.0.1',
+  name: 'Literally nothing',
+};
+
+// If you add new functions anywhere inside of a layer, and those functions have an effect when called, add them here.
+// (The ones here are examples, all official functions are already taken care of)
+var doNotCallTheseFunctionsEveryTick = ['blowUpEverything'];
+
+function getStartPoints() {
+  return new Decimal(modInfo.initialStartPoints);
+}
+
+// Determines if it should show points/sec
+function canGenPoints() {
+  return hasUpgrade('p', 11);
+}
+
+// Calculate points/sec!
+function getPointGen() {
+  if (!canGenPoints()) return new Decimal(0);
+
+  let gain = new Decimal(1);
+  if (hasUpgrade('p', 12)) gain = gain.times(upgradeEffect('p', 12));
+  return gain;
+}
+
+// You can add non-layer related variables that should to into "player" and be saved here, along with default values
+function addedPlayerData() {
+  return {};
+}
+
+// Display extra things at the top of the page
+var displayThings = ['Endgame: e280000000 points'];
+
+// Determines when the game "ends"
+function isEndgame() {
+  return player.points.gte(new Decimal('e280000000'));
+}
+
+// Less important things beyond this point!
+
+// You can change this if you have things that can be messed up by long tick lengths
+function maxTickLength() {
+  return 3600000; // Default is 1 hour which is just arbitrarily large
+}
